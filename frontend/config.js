1ded598fb9c17bf89fe926d2f2ff3e7f6e1eecc9
L.Rustam <kallycta2@yandex.ru>
2025-09-19 14:53:33 +0300

    url front

--- frontend/config.js
+++ frontend/config.js
@@ -6,6 +6,7 @@ const CONFIG = {
   },
   production: {
     // Yandex Cloud API Gateway URL
+        // Yandex Cloud API Gateway URL
     API_BASE_URL: 'https://d5d7374m2hsrgpo1quas.apigw.yandexcloud.net',
     API_KEY: '881f8e616c8deb37a9eb7c485c3dcee306dae17656bed5b4b8783549300fcaac'
   }
