66b43f10a05399d9ec00ed8a6665c72041b55360
L.Rustam <kallycta2@yandex.ru>
2025-09-19 18:58:07 +0300

    auto deploy Yandex CLoud

--- frontend/config.js
+++ frontend/config.js
@@ -1,12 +1,12 @@
 // Конфигурация для разных средfdf
 const CONFIG = { 
   development: {
-    API_BASE_URL: 'http://localhost:3000/api',
+    API_BASE_URL: 'http://localhost:3000',
     API_KEY: '881f8e616c8deb37a9eb7c485c3dcee306dae17656bed5b4b8783549300fcaac'
   },
   production: {
-    // Yandex Cloud API Gateway URL
-    API_BASE_URL: 'https://d5digcd630nfv5evp48l.svoluuab.apigw.yandexcloud.net', 
+    // Временно используем локальный сервер для тестирования
+    API_BASE_URL: 'http://localhost:3000', 
     API_KEY: '881f8e616c8deb37a9eb7c485c3dcee306dae17656bed5b4b8783549300fcaac'
   }
 };
